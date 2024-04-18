d2fca3195f676116915f5d038c7bd3876e240fba
ItzTas <talessabini2@gmail.com>
2024-04-18 19:51:17 -0300

    I created a deck class

---
+++ src/deck.py
@@ -0,0 +1,18 @@
+import random
+
+class Deck():
+    def __init__(self) -> None:
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        self._cards = [(suit, rank) for suit in suits for rank in ranks]
+        
+    def shuffle_deck(self) -> None:
+        random.shuffle(self._cards)
+        
+    def pick_card(self) -> None:
+        return self._cards.pop()
+    
+    def renitialize_deck(self) -> None:
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        self._cards = [(suit, rank) for suit in suits for rank in ranks]
