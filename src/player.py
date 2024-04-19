13b02036104351e59d0fc93ddffb6da3b46c53b0
ItzTas <talessabini2@gmail.com>
2024-04-18 21:37:19 -0300

    I created the calculate hand method

--- src/player.py
+++ src/player.py
@@ -10,6 +10,27 @@ class Player():
     
     def lose_points(self, penalty) -> None:
         self.points -= penalty
+        
+    def gain_points(self, gain) -> None:
+        self.points += gain
     
     def calculate_hand(self) -> int:
-        pass
+        total_score = 0
+        first_ace = False
+        for card in self._hand:
+            rank = card[1]
+            if rank.isdigit():
+                total_score += int(rank)
+            elif rank != "Ace":
+                total_score += 10
+            else:
+                if not first_ace:
+                    first_ace = True
+                else:
+                    total_score += 1
+        
+        if first_ace and total_score + 11 <= 21:
+            total_score += 11
+        elif first_ace:
+            total_score += 1
+        return total_score
